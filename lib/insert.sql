a7d2828dab47697eea5736c5cf21cacc047b542d
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-29 22:53:13 -0600

    commit tree fbf69fba0ee397f466c77198e4269d965b0aff25

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 4, "F", "brown", "nice", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 4, "F", "brown", "nice", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 4, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 4, "F", "brown", "nice", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (6, "Grinch", 4, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (7, "Wendy", 4, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (8, null, 4, "F", "brown", "nice", 1);
